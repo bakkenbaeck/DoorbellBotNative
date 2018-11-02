ef139263a416dc1290fb9654515dbc71fc32f770
Anders <anders@bakkenbaeck.no>
2018-11-02 11:54:26 +0100

    Connect Android project to shared code

--- android/src/main/java/com/bakkenbaeck/doorbellbotnative/MainActivity.kt
+++ android/src/main/java/com/bakkenbaeck/doorbellbotnative/MainActivity.kt
@@ -2,11 +2,19 @@ package com.bakkenbaeck.doorbellbotnative
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.mainText
+import com.bakkenbaeck.doorbellbotnative.common.createApplicationScreenMessage
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        mainText.text = createApplicationScreenMessage()
     }
 }
