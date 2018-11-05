25b9663dea818006a778ed73864bf53d3562e4f5
Ellen Shapiro <designatednerd@gmail.com>
2018-11-05 11:11:12 +0100

    Kill UI test target (going to replace with Earl Grey), fix naming on main test target

---
+++ iOS/DoorbellBotNativeTests/DoorbellBotNativeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Doorbell_bot_NativeTests.swift
+//  Doorbell-bot NativeTests
+//
+//  Created by Anders Skaalsveen on 02/11/2018.
+//  Copyright © 2018 Bakken &/ Bæck. All rights reserved.
+//
+
+import XCTest
+@testable import DoorbellBotNative
+
+class DoorbellBotNativeTests: XCTestCase {
+    
+    func testsShouldBeImplemented() {
+        XCTFail("We should probably add some tests here")
+    }
+}
