be8c69fd97975c45ec3a7ae0cf22b8ce47547679
Ellen Shapiro <designatednerd@gmail.com>
2018-11-05 11:18:32 +0100

    Add native UI test target and pods

---
+++ iOS/DoorbellBotNativeUITests/Robots/BasicRobot.swift
@@ -0,0 +1,9 @@
+//
+//  BasicRobot.swift
+//  DoorbellBotNativeUITests
+//
+//  Created by Ellen Shapiro on 11/5/18.
+//  Copyright © 2018 Bakken &/ Bæck. All rights reserved.
+//
+
+import Foundation
