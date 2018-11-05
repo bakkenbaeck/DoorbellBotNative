8a70f707e7f49df50628ebd27522ce3d76013195
Ellen Shapiro <designatednerd@gmail.com>
2018-11-05 11:02:17 +0100

    kill off a bunch of boilerplate

--- iOS/DoorbellBotNative/ViewController.swift
+++ iOS/DoorbellBotNative/ViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 import SharedCode
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private var testLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        initialize()
-    }
-
-    func initialize() {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.font = label.font.withSize(25)
-        label.text = CommonKt.createApplicationScreenMessage()
-        view.addSubview(label)
+        self.testLabel.text = CommonKt.createApplicationScreenMessage()
     }
 }
 
